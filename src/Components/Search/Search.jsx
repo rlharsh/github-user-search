c2f6205f0a7fabafeb0742863e476a2d05291866
rlharsh <114967560+rlharsh@users.noreply.github.com>
2023-05-26 18:44:01 -0500

    Update accessibility

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -15,10 +15,10 @@ const Search = ( { searchUser, error }) => {
     }
 
     return (
-        <section className="search-container">
+        <section className={`search-container  ${theme === 'light' ? 'light' : ''}`} aria-label='Search Container'>
             <form className="search-form" onSubmit={handleSubmit}>
                 <input className={theme} type="text" aria-label='Search' name="username" id="username" placeholder='Search Github username...' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-                <img className="search-icon" src={Glass} />
+                <img className="search-icon" src={Glass} alt='Search Icon' />
                 <button className='search-button' type='submit' aria-label='Search Button'>Search</button>
                 <p className={`not-found ${error === true ? 'not-found__visible' : ''}`}>No Results</p>
             </form>
